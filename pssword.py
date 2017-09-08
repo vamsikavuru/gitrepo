a47c87f8375d598564f0f899286f913f560c78bd
sunil <kvk@gmail.com>
2017-09-08 20:48:54 +0530

    password prog

---
+++ pssword.py
@@ -0,0 +1,12 @@
+
+while True :
+  name = input("enter  name")
+  if name != 'satya'
+  continue
+  password = input("hello, Vamsi what's your password?")
+  if password == "devops"
+     break
+
+  print("Access Denied")
+end
+
